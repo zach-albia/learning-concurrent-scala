ab434934c2a75aa7913e7094d429048fdc4bd002
zach.albia <zach.albia@gmail.com>
2020-09-16 18:09:01 +0800

    add ch4 exercises 3-5

--- src/main/scala/org/learningconcurrency/ch4/Exercises.scala
+++ src/main/scala/org/learningconcurrency/ch4/Exercises.scala
@@ -1,10 +1,12 @@
 package org.learningconcurrency.ch4
 
+import scala.async.Async.{async, await}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future, Promise}
 import scala.io.Source
 import scala.io.StdIn.readLine
+import scala.util.{Failure, Success}
 
 object Exercise1 extends App {
   print("Enter your URL: ")
@@ -55,4 +57,27 @@ object Exercise2 extends App {
   val a = new IVar[Int]
   a := 4
   println(a())
+}
+
+object Exercise3 {
+  implicit class FutureOps[T](f: Future[T]) {
+    def exists(p: T => Boolean): Future[Boolean] = f.map(p)
+  }
+}
+
+object Exercise4 {
+  implicit class FutureOps[T](f: Future[T]) {
+    def exists(pred: T => Boolean): Future[Boolean] = {
+      val p = Promise[Boolean]
+      f.onComplete { case Success(t) => p.success(pred(t)) }
+      p.future
+    }
+  }
+}
+
+object Exercise5 {
+  implicit class FutureOps[T](f: Future[T]) {
+    def exists(pred: T => Boolean): Future[Boolean] =
+      async { pred(await(f)) }
+  }
 }
